f1a346826126211b5a289216a001fd994ead45ed
DieseEier <87245568+DieseEier@users.noreply.github.com>
2021-08-03 19:20:54 +0200

    println < System.out.println
    mikasa lügt

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from colorama import Fore, Back, Style
 from os import system
 import os
 
+def println(WIEGEHTDAS):
+    print(WIEGEHTDAS)
+
 os.system('cls||clear')
+
 banner = f'''{Fore.LIGHTCYAN_EX}
                 ███╗   ██╗ █████╗ ███╗   ███╗███████╗     ██████╗ ███████╗███╗   ██╗
                 ████╗  ██║██╔══██╗████╗ ████║██╔════╝    ██╔════╝ ██╔════╝████╗  ██║
@@ -15,40 +19,32 @@ banner = f'''{Fore.LIGHTCYAN_EX}
                 
                                     Version: 100.0
                                     Developer: maquzo#6314
-				    Mikasa: lügt
+                                    Contributer: Bolusterbuster_710
                                     https://discord.gg/f4YcqekfBe                          
 '''
-print(banner)
+println(banner)
 title = "NAME-GENERATOR"
 system("title "+title)
 
-vowels = ["a", "e", "i", "o", "u", "du hurensohn"]
+vowels = ["a", "e", "i", "o", "u"]
 
 is_5_letter = random.getrandbits(1)
 is_5_letter = bool(is_5_letter)
 
 while True:
-	input_str = input("Please enter 3 letters of you choice: ")
-	if len(input_str) != 3:
-    		print("Error! Only 3 characters allowed!")
-    		sys.exit()
+    input_str = input("Please enter 3 letters of you choice: ")
+    if len(input_str) != 3:
+            println("Error! Only 3 characters allowed!")
+            sys.exit()
 
-	random_letter = random.choice(vowels)
+    random_letter = random.choice(vowels)
 
-	/* 
-	was
-	*/
 
-	if is_5_letter:
-    		random_5_letter = random.choice(vowels)
-    		print("Your name is: " + random_letter + input_str + random_5_letter) // was
-    		sys.exit()
+    if is_5_letter:
+            random_5_letter = random.choice(vowels)
+            println("Your name is: " + random_letter + input_str + random_5_letter)
+            continue
 
-	/* 
-	was
-	*/
-
-	else:
-    		print("Your name is: " + input_str + random_letter)
-
+    else:
+            println("Your name is: " + input_str + random_letter)
 
