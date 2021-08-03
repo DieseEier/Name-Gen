a9f0cc4d29ebac02aa07b987ce2e5486eaf1770c
DieseEier <87245568+DieseEier@users.noreply.github.com>
2021-08-03 18:42:06 +0200

    hi
    mikasa lpgt

--- main.py
+++ main.py
@@ -13,31 +13,42 @@ banner = f'''{Fore.LIGHTCYAN_EX}
                 ██║ ╚████║██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝███████╗██║ ╚████║
                 ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝
                 
-                                    Version: 1.0
+                                    Version: 100.0
                                     Developer: maquzo#6314
+				    Mikasa: lügt
                                     https://discord.gg/f4YcqekfBe                          
 '''
 print(banner)
 title = "NAME-GENERATOR"
 system("title "+title)
 
-vowels = ["a", "e", "i", "o", "u"]
+vowels = ["a", "e", "i", "o", "u", "du hurensohn"]
+
 is_5_letter = random.getrandbits(1)
 is_5_letter = bool(is_5_letter)
 
-input_str = input("Please enter 3 letters of you choice: ")
-if len(input_str) > 3:
-    print("Error! Only 3 characters allowed!")
-    sys.exit()
-if len(input_str) < 3:
-    print("Error! Only 3 characters allowed!")
-    sys.exit()
+while True:
+	input_str = input("Please enter 3 letters of you choice: ")
+	if len(input_str) != 3:
+    		print("Error! Only 3 characters allowed!")
+    		sys.exit()
 
-random_letter = random.choice(vowels)
-if is_5_letter:
-    random_5_letter = random.choice(vowels)
-    print("Your name is: " + random_letter + input_str + random_5_letter)
-    sys.exit()
-else:
-    print("Your name is: " + input_str + random_letter)
-    sys.exit()
+	random_letter = random.choice(vowels)
+
+	/* 
+	was
+	*/
+
+	if is_5_letter:
+    		random_5_letter = random.choice(vowels)
+    		print("Your name is: " + random_letter + input_str + random_5_letter) // was
+    		sys.exit()
+
+	/* 
+	was
+	*/
+
+	else:
+    		print("Your name is: " + input_str + random_letter)
+
+
